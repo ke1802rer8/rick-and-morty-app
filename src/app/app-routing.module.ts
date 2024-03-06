77f3c10aa31c2c73a58fd6ff480172832fa25168
Ekaterina Roshka <45431245+ke1802rer8@users.noreply.github.com>
2024-03-06 11:30:42 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
-  { path: 'heroes', component: HeroesComponent},
-  { path: 'detail/:id', component: HeroDetailsComponent }
+  { path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
